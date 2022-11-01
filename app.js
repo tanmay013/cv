58e98f5dbb45ef712052e4ed337f829ad66b232c
tanmay013 <59093235+tanmay013@users.noreply.github.com>
2022-11-01 14:03:27 +0530

    version-02

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
+          
+          
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
+            document
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
